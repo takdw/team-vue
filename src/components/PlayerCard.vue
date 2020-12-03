b51e8dbf53673a69ceb746dcb6c67565617dd1e0
Tamrat Assefa Kessito <mamushassefa@gmail.com>
2020-12-04 01:45:31 +0300

    add the time the player was added

--- src/components/PlayerCard.vue
+++ src/components/PlayerCard.vue
@@ -15,12 +15,17 @@
       <h3 class="text-base font-medium text-gray-800">
         {{ player.first_name + " " + player.last_name }}
       </h3>
-      <p>{{ player.age }}</p>
+      <p class="font-medium">{{ player.age }}</p>
+      <p class="mt-2 text-gray-600 text-xs">
+        <em>added {{ added }}</em>
+      </p>
     </div>
   </div>
 </template>
 
 <script>
+import { DateTime } from "luxon";
+
 export default {
   props: {
     player: {
@@ -34,6 +39,9 @@ export default {
   computed: {
     name() {
       return this.player.first_name + " " + this.player.last_name;
+    },
+    added() {
+      return DateTime.fromISO(this.player.created_at).toRelative();
     },
   },
   methods: {},
